08672c58d17bfc404d37dc201ac6cf0b0f9f7e2a
jamesl1030 <jalucas1@student.fullsail.edu>
2021-09-05 14:54:31 -0400

    updated WeatherAPI function

--- weatherapp/src/components/APIOutput.js
+++ weatherapp/src/components/APIOutput.js
@@ -3,12 +3,9 @@ import React from 'react'
 const APIOutput = props => {
     return (   
         <div>
-        
             <h1>{props.weatherMain}</h1>
             <h1>{props.weatherDescription}</h1>
-            <h1>{props.weatherIcon}</h1>
- 
-     
+            <h1>{props.weatherIcon}</h1>  
         </div>
     )
 }
--- weatherapp/src/pages/WeatherAPI.js
+++ weatherapp/src/pages/WeatherAPI.js
@@ -3,13 +3,13 @@ import APIOutput from '../components/APIOutput';
 import {useState, useEffect} from 'react';
 
 function WeatherAPI() {
-    const [weatherData, setuserData] = useState(null);
+    const [weatherData, setWeatherData] = useState(null);
     useEffect(() => {
         async function fetchAPI(){
             const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Florida&appid=108bed69d959699fe0a38d8164dbc3bb');
             const data = await response.json();
             const [currentWeather] = data.weather;
-            setuserData(currentWeather);
+            setWeatherData(currentWeather);
         }
         fetchAPI();
     }, []);
