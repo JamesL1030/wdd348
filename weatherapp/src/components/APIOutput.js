44a915311bc7c370936dc1a7c8caebf5c3c6a50d
jamesl1030 <jalucas1@student.fullsail.edu>
2021-09-09 21:08:41 -0400

    updated

--- weatherapp/src/components/APIOutput.js
+++ weatherapp/src/components/APIOutput.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header'
 
 const APIOutput = props => {
     return (   
-        <div>
-            <h1>{props.weatherMain}</h1>
-            <h1>{props.weatherDescription}</h1>
-            <h1>{props.weatherIcon}</h1>  
+        <div style={styles.container}>   
+            <Header />    
+            <h2>{props.locationName}</h2>
+            <div style={styles.myBox}>
+                <h1>{props.weatherTemp}&deg;</h1>
+                <h1>{props.weatherMain}</h1>
+                <h1>{props.weatherDescription}</h1>
+                <h1>{props.weatherIcon}</h1>  
+            </div>
         </div>
     )
 }
 
 export default APIOutput
+//Styles
+const styles = {
+    container: {
+        backgroundColor: 'orange',
+        height: '100vh',
+    },
+    myBox: {
+        margin: 'auto',
+        height: '250px',
+        width: '98%',
+        backgroundColor: '#fff'
+    }
+}
--- weatherapp/src/components/UserInput.js
+++ weatherapp/src/components/UserInput.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import MiamiAPI from '../pages/MiamiAPI';
 import OrmondBeachAPI from '../pages/OrmondBeachAPI';
+import BaltimoreAPI from '../pages/BaltimoreAPI';
+import OrlandoAPI from '../pages/OrlandoAPI';
+import DenverAPI from '../pages/DenverAPI';
+import HagerstownAPI from '../pages/HagerstownAPI';
 
 function UserInput() {
     return(
         
         <div>
             <MiamiAPI />
+            <BaltimoreAPI />
             <OrmondBeachAPI />
+            <OrlandoAPI />
+            <DenverAPI />
+            <HagerstownAPI />
+            
         </div>
     )
 };
