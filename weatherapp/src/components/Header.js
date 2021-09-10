42baa3a2512464223868a4f06470f623787acfd7
jamesl1030 <jalucas1@student.fullsail.edu>
2021-09-09 21:07:05 -0400

    initial commit

---
+++ weatherapp/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header(props) {
+    return (
+        <div style={styles.headingContainer}>
+                     
+        </div>
+    );
+}
+
+export default Header
+//Styles
+const styles = {
+    headingContainer: {
+        display: 'flex',
+        borderBottom: '1px solid #232323',
+        padding: '1.2em',
+        height: '100px',
+        justifyContent: 'space-between',
+        backgroundColor: 'green'
+    }
+
+}
