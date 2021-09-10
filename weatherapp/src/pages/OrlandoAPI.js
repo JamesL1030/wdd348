76528a1c6856c35c0fc5531f89611e7788726a1a
jamesl1030 <jalucas1@student.fullsail.edu>
2021-09-09 21:04:23 -0400

    API added

---
+++ weatherapp/src/pages/OrlandoAPI.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import APIOutput from '../components/APIOutput';
+import {useState, useEffect} from 'react';
+
+function OrlandoAPI() {
+    const [weatherData, setWeatherData] = useState(null);
+    useEffect(() => {
+        async function fetchAPI(){
+            const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=108bed69d959699fe0a38d8164dbc3bb&&units=imperial');
+            const data = await response.json();
+            const currentWeather = data;
+            setWeatherData(currentWeather);
+        }
+        fetchAPI();
+    }, []);
+
+    return (
+        <div>          
+            {weatherData &&<APIOutput 
+                locationName={weatherData.name}
+                weatherTemp={weatherData.main.temp_max}
+                weatherMain={weatherData.weather.main}
+                weatherDescription={weatherData.weather.description}  
+                weatherIcon={weatherData.weather.icon}  
+            />}
+        </div>
+    );
+}
+
+export default OrlandoAPI;
