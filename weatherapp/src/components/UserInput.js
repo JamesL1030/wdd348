16d6afef625857c8e190be96fefc745536de0525
jamesl1030 <jalucas1@student.fullsail.edu>
2021-09-12 20:11:30 -0400

    components added

--- weatherapp/src/components/UserInput.js
+++ weatherapp/src/components/UserInput.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import MiamiAPI from '../pages/MiamiAPI';
-import OrmondBeachAPI from '../pages/OrmondBeachAPI';
-import BaltimoreAPI from '../pages/BaltimoreAPI';
-import OrlandoAPI from '../pages/OrlandoAPI';
-import DenverAPI from '../pages/DenverAPI';
-import HagerstownAPI from '../pages/HagerstownAPI';
+import Header from '../components/Header';
+import InputBox from './InputBox';
+import MyButton from './MyButton';
+import GridBaltimoreAPI from './GridBaltimoreAPI';
+import GridOrlandoAPI from './GridOrlandoAPI';
+import GridDenverAPI from './GridDenverAPI';
+import GridHagerstownAPI from './GridHagerstownAPI';
+import GridMiamiAPI from './GridMiamiAPI';
+import GridOrmondAPI from './GridOrmondAPI';
 
 function UserInput() {
-    return(
-        
-        <div>
-            <MiamiAPI />
-            <BaltimoreAPI />
-            <OrmondBeachAPI />
-            <OrlandoAPI />
-            <DenverAPI />
-            <HagerstownAPI />
-            
+    return(     
+        <div style={styles.container}>
+            <Header />
+            <div style={styles.inputBox}>
+                <InputBox  style={styles.inputBox} placeholderText='Search' />
+            </div>
+            <MyButton buttonText='Search' />
+            <div style={styles.row}>
+                <GridMiamiAPI />
+                <GridOrmondAPI />
+            </div>
+            <div style={styles.row}>
+                <GridBaltimoreAPI />
+                <GridOrlandoAPI />
+            </div>
+            <div style={styles.row}>
+                <GridDenverAPI />
+                <GridHagerstownAPI />
+            </div> 
         </div>
     )
 };
 
 export default UserInput;
+//Styles
+const styles = {
+    container: {
+        backgroundColor: '#203647',
+        height: '100vh'
+    },
+    row: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    inputBox: {
+        margin: '1em 0'
+    }
+}
